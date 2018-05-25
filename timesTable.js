2f951c84e77ce5c86394d306610ee24665bc4fb5
makesler <39626003+makesler@users.noreply.github.com>
2018-05-25 09:33:00 -0500

    Create timesTable.js

---
+++ timesTable.js
@@ -0,0 +1,10 @@
+  // i = factor 1
+  // x = factor 2
+
+for (i=0; i<11; i++) {
+  console.log();
+  console.log(i + " TIMES TABLE");
+  for (n=0; n<11; n++) {
+    console.log(i + " x " + n + " = " + i*n);
+  }
+}
