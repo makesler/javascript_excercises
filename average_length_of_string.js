c9dcbdc0592bf5706306c2683244786b9ec3a5ad
makesler <39626003+makesler@users.noreply.github.com>
2018-05-25 09:18:28 -0500

    Create average_length_of_string.js

---
+++ average_length_of_string.js
@@ -0,0 +1,10 @@
+/* takes two words and finds the average length of both */
+
+function computeAverageLengthOfWords(word1, word2) {
+  // your code here
+  var n = word1.length;
+  var n2 = word2.length;
+  return (n + n2)/2;
+}
+
+computeAverageLengthOfWords('code', 'programs');
