58a55dbaa1cacdcc10f6642309d5b4d5571636b5
makesler <39626003+makesler@users.noreply.github.com>
2018-05-25 09:30:55 -0500

    Create objectIntroduction.js

---
+++ objectIntroduction.js
@@ -0,0 +1,14 @@
+var library = [ 
+   { author: 'Bill Gates', title: 'The Road Ahead', libraryID: 1254},
+   { author: 'Steve Jobs', title: 'Walter Isaacson', libraryID: 4264},
+   { author: 'Aldous Huxley', title: 'Brave New World', libraryID: 3245},
+   { author: 'Stephen Hawking', title: 'A Brief History of Time', libraryID: 8237},
+   { author: 'Frank Herbert', title: 'Dune', libraryID: 390},
+   { author: 'Herman Hesse', title: 'Siddhartha', libraryID: 2112}
+   ];
+   
+function libSort(a, b) {
+  return a.libraryID - b.libraryID;
+}
+   
+console.log(library.sort(libSort));
